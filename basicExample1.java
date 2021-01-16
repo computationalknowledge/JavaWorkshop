77811eadc8842839008b8be1b13ce6d6ece2a39e
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-01-16 13:39:42 -0500

    Create basicExample1.java

---
+++ basicExample1.java
@@ -0,0 +1,12 @@
+public class Calculator{
+
+     public static void main(String []args){
+        System.out.println( add(2,2) );
+     }
+     
+     public static int add(int number1, int number2){
+         
+         return number1 + number2;
+         
+     }
+}
