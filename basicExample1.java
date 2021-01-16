50ae586411c0c65cd40363127ce87fce11f7f890
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-01-16 14:19:36 -0500

    Update basicExample1.java

--- basicExample1.java
+++ basicExample1.java
@@ -2,41 +2,37 @@ public class Manager{
 
      public static void main(String []args){
         Calculator c = new Calculator();
-        // c.multiplicationTable(0, 10);
         c.dogAndCat(0, 10);
      }
      
 }
 
 
-class PlayGround{
-    public void ForLoops(){
-        
-        
-        
-    }
-}
-
 class Calculator{
     
-      public float add(float number1, float number2){
-         return number1 + number2;    }
+      public float add(float number1, float number2){return number1 + number2;    }
    
-      public float subtract(float number1, float number2){
-         return number1 - number2;    }
+      public float subtract(float number1, float number2){return number1 - number2;    }
     
-      public float multiply(float number1, float number2){
-         return number1 * number2;    }
+      public float multiply(float number1, float number2){return number1 * number2;    }
          
-      public float divide(float number1, float number2){
-         return number1 / number2;    }
+      public float divide(float number1, float number2){ return number1 / number2;    }
 
       public void dogAndCat(int start, int end){
-          // practice using a for loop
-          for (int i = 0; i<10 ; i++){
-              System.out.println(i);
-          }
-          
-    
+           
+     
+        for (int i = start; i < end ; i++){
+            
+             for (int j = start; j < end ; j++){
+                 
+                 // #to: use if statements to break the out put to 
+                 // to make a nicely formatted table
+                 System.out.println(i*j);
+                 
+             }
+            
+        }       
+           
+
       }    
 }
